9928c33d47366760e7cc3352e53c8d3e3c78a9a3
muhsinazmal <muhsinstudent12@gmail.com>
2023-02-05 15:40:38 +0600

    completed but not responsive

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ jQuery(document).ready(function($) {
         time: 1000
     });
 });
+
+// STICKY HEADER START
+$(window).scroll(function(){
+    var scrolling = $(this).scrollTop();
+    
+    if(scrolling > 200){
+        $('.navbar').addClass('sticky_header');
+    }
+    else {
+        $('.navbar').removeClass('sticky_header');
+    }
+    });  
+// STICKY HEADER END
