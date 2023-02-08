b2f093834065b841bc6d7c39a3c06fbcc7f34e44
muhsinazmal <muhsinstudent12@gmail.com>
2023-02-08 18:43:56 +0600

    relatedGamesPart

--- js/script.js
+++ js/script.js
@@ -92,6 +92,20 @@ $(function () {
 
 $('.variableW').slick({
     infinite: false,
-    speed: 300,
-    slidesToShow: 3
-  });
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                autoplay: true,
+                autoplaySpeed: 1000,
+                centerMode: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        },
+    ]
+});
